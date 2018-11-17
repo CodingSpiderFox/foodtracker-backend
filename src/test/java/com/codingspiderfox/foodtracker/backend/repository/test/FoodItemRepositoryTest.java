9321f0bf2e649d51960bdbfd8243aee22827c0a4
CodingSpiderFox <sunrisechain@gmail.com>
2018-11-17 16:28:01 +0100

    add tests to store and retrieve food item

--- src/test/java/com/codingspiderfox/foodtracker/backend/repository/test/FoodItemRepositoryTest.java
+++ src/test/java/com/codingspiderfox/foodtracker/backend/repository/test/FoodItemRepositoryTest.java
@@ -3,13 +3,17 @@ package com.codingspiderfox.foodtracker.backend.repository.test;
 import com.codingspiderfox.foodtracker.backend.entities.FoodItem;
 import com.codingspiderfox.foodtracker.backend.repository.FoodItemRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@DataMongoTest
+@RunWith(SpringRunner.class)
 public class FoodItemRepositoryTest {
 
     @Autowired
@@ -27,6 +31,5 @@ public class FoodItemRepositoryTest {
         assertThat(retrievedFoodItem.isPresent())
                 .as("Retrieved food item should not be null")
                 .isTrue();
-
     }
 }
